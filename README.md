a39186a6f62a976a00e09639a41f504fbbc900e1
Joao Dias <joaogomesdias2001@gmail.com>
2022-03-12 16:35:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## projeto novo (não finalizado)
+
+fazendo um landing page de um site chamado 'Color', por agora só temos a navbar.
+
+Live Site - [Color](https://joaodiazz.github.io/y_Color/)
---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#mobile__menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
